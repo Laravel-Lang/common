58d0d936968c6d9904878aeaf0ff14f03873286c
Andrey Helldar <helldar@dragon-code.pro>
2023-02-12 14:11:25 +0300

    Added phpunit file

---
+++ phpunit.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the "Laravel-Lang/publisher" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@dragon-code.pro>
+ * @copyright 2022 Andrey Helldar
+ * @license MIT
+ *
+ * @see https://github.com/Laravel-Lang/publisher
+ */
+
+/*
+  |--------------------------------------------------------------------------
+  | Register The Composer Auto Loader
+  |--------------------------------------------------------------------------
+  |
+  | Composer provides a convenient, automatically generated class loader
+  | for our application. We just need to utilize it! We'll require it
+  | into the script here so that we do not have to worry about the
+  | loading of any our classes "manually". Feels great to relax.
+  |
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+/*
+  |--------------------------------------------------------------------------
+  | Set The Default Timezone
+  |--------------------------------------------------------------------------
+  |
+  | Here we will set the default timezone for PHP. PHP is notoriously mean
+  | if the timezone is not explicitly set. This will be used by each of
+  | the PHP date and date-time functions throughout the application.
+  |
+ */
+
+date_default_timezone_set('UTC');
